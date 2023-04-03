e085994ffe3824c4f559a3c0f5ee730ee33d9a1c
liaorui <liaor@suntang.com>
2023-04-03 10:09:03 +0800

    增加注释

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+// 解决重复点击路由报错的BUG
 // 下面这段代码主要解决这个问题 ：Uncaught (in promise) Error: Redirected when going from "/login" to "/index" via a navigation guard.
 const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
